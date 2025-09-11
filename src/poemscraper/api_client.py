f20ec92468c9e9cf710f504c3542183ea44f76ff
sharle4 <charleskayssieh@gmail.com>
2025-09-11 11:01:47 +0200

    3.4.2 version : tree comparator added

--- src/poemscraper/api_client.py
+++ src/poemscraper/api_client.py
@@ -8,7 +8,7 @@ import backoff
 logger = logging.getLogger(__name__)
 
 WIKIMEDIA_USER_AGENT = (
-    "WikisourcePoemScraper/3.4.1 (https://github.com/sharle4/wikisource-poem-scraper; charleskayssieh@gmail.com) "
+    "WikisourcePoemScraper/3.4.2 (https://github.com/sharle4/wikisource-poem-scraper; charleskayssieh@gmail.com) "
     "aiohttp/" + aiohttp.__version__
 )
 

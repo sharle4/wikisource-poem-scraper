029199b3686e840ef62fd230df044e78902fe9fa
sharle4 <charleskayssieh@gmail.com>
2025-09-15 13:03:17 +0200

    3.9.3 version : explorer no data bug solved

--- src/poemscraper/api_client.py
+++ src/poemscraper/api_client.py
@@ -8,7 +8,7 @@ import backoff
 logger = logging.getLogger(__name__)
 
 WIKIMEDIA_USER_AGENT = (
-    "WikisourcePoemScraper/3.9.2 (https://github.com/sharle4/wikisource-poem-scraper; charleskayssieh@gmail.com) "
+    "WikisourcePoemScraper/3.9.3 (https://github.com/sharle4/wikisource-poem-scraper; charleskayssieh@gmail.com) "
     "aiohttp/" + aiohttp.__version__
 )
 

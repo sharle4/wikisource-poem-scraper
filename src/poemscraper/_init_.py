8c0a42c1e2739d785144c2d219be8b7752c27c31
sharle4 <charleskayssieh@gmail.com>
2025-10-18 19:03:36 +0200

    4.3.2 version : gitignore edited

--- src/poemscraper/_init_.py
+++ src/poemscraper/_init_.py
@@ -1,4 +1,4 @@
 """
 Wikisource Poem Scraper Package.
 """
-version = "4.3.1"
+version = "4.3.2"
--- src/poemscraper/api_client.py
+++ src/poemscraper/api_client.py
@@ -8,7 +8,7 @@ import backoff
 logger = logging.getLogger(__name__)
 
 WIKIMEDIA_USER_AGENT = (
-    "WikisourcePoemScraper/4.3.1 (https://github.com/sharle4/wikisource-poem-scraper; charleskayssieh@gmail.com) "
+    "WikisourcePoemScraper/4.3.2 (https://github.com/sharle4/wikisource-poem-scraper; charleskayssieh@gmail.com) "
     "aiohttp/" + aiohttp.__version__
 )
 
